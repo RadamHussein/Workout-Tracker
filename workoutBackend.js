99d5695e21a33c51282d3a2ba1ac342dc0fa15dd
Adam Smith <stupidalphabet@gmail.com>
2016-03-10 19:44:01 -0600

    added /insert function

--- workoutBackend.js
+++ workoutBackend.js
@@ -4,11 +4,63 @@ var app = express();
 
 app.set('port', 3000);
 
+//connects to the database
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+/*
+//make table with desired columns
+CREATE TABLE workouts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    reps INT,
+    weight INT,
+    date DATE,
+    lbs BOOLEAN
+);
+*/
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.get('/insert',function(req,res,next){
+  console.log("/insert has been called");
+  var context = {};
+  pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.c], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.type('text/plain'); //delete this once done testing
+    res.send('An entry has been added to the table');
+  });
+});
+
 app.get('/',function(req,res){
   res.type('text/plain');
   //res.type('application/json');
   res.send('Welcome to the main page!');
-  console.log("I jave just been called");
+  console.log("I jave just been called"); //delete this in the final version
 });
 
 app.use(function(req,res){
--- workoutFrontend.js
+++ workoutFrontend.js
@@ -2,9 +2,23 @@ document.addEventListener("DOMContentLoaded", main);
 
 function main(){
 	var req = new XMLHttpRequest();
-	req.open("GET", "http://52.33.123.66:3000/");
-	var res = req.response;
-	console.log(res);
-	console.log("prove that this front end is working");
-	document.getElementById("response").textContent = "prove that this works";
-}
+	req.open("GET", "http://52.33.123.66:3000/", true);
+	req.addEventListener("load", function(){
+		var res = req.response;
+		console.log(res);
+		document.getElementById("response").textContent = res;
+	});
+	req.send();
+};
+
+document.getElementById("add").addEventListener("click", function(event){
+		var name = document.getElementById("name").value;
+		var req = new XMLHttpRequest();
+		req.open("GET", "http://52.33.123.66:3000/insert", true);
+		req.addEventListener("load", function(){
+			var res = req.response;					
+			document.getElementById("response").textContent = res;
+		});
+		event.preventDefault();
+});
+	
