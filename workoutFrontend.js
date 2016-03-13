bb02a5e095e45c158a120d7bf4d00f37752a150c
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 16:42:34 -0500

    trying new way to send request

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main(){
-	var req = new XMLHttpRequest();
-	req.open("GET", "http://52.33.123.66:3000/", true);
-	req.withCredentials = true;
-	req.addEventListener("load", function(){
-		var res = req.response;
+	var req1 = new XMLHttpRequest();
+	req1.open("GET", "http://52.33.123.66:3000/", true);
+	req1.withCredentials = true;
+	req1.addEventListener("load", function(){
+		var res = req1.response;
 		console.log(res);
 		document.getElementById("response").textContent = res;
 	});
-	req.send(null);
+	req1.send(null);
 
 
 document.getElementById("add").addEventListener("click", function(event){
