12da18b50845fbae7942dfb52ae6b7ea20e7c9b9
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 12:14:38 -0500

    no comment

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -1,19 +1,3 @@
-/*
-var ec2 = new AWS.EC2({region: 'us-west-2'});
- 
-ec2.describeInstances(function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    data.Reservations.forEach(function(reservation) {
-      reservation.Instances.forEach(function(instance) {
-        console.log(instance.InstanceId);
-      });
-    });
-  }
-});
-*/
-
 document.addEventListener("DOMContentLoaded", main);
 
 function main(){
@@ -26,6 +10,7 @@ function main(){
 	});
 	req.send(null);
 
+/*
 document.getElementById("add").addEventListener("click", function(event){
 		console.log("click is working");
 		var name = document.getElementById("name").value;
@@ -37,54 +22,5 @@ document.getElementById("add").addEventListener("click", function(event){
 		});
 		event.preventDefault();
 	});
-};
-
-
-/*
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
-
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-}
-
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // All HTML5 Rocks properties support CORS.
-  var url = "http://52.33.123.66:3000/";
-
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-}
 */
+};
