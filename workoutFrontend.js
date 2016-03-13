fc298feeb43f5a1377ff0b53a3b3b2e9c212f6c0
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 16:48:33 -0500

    removed null

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -9,23 +9,23 @@ function main(){
 		console.log(res);
 		document.getElementById("response").textContent = res;
 	});
-	req1.send(null);
+	req1.send();
 
 
 document.getElementById("add").addEventListener("click", function(event){
 		console.log("click is working");
 		var name = document.getElementById("name").value;
-		var req = new XMLHttpRequest();
-		req.open("GET", "http://52.33.123.66:3000/insert", name, reps, weight, date, lbs, true);
-		req.addEventListener("load", function(){
-			if (req.status >= 200 && req.status < 400){
-				var res = req.response;					
+		var req2 = new XMLHttpRequest();
+		req2.open("GET", "http://52.33.123.66:3000/insert", name, reps, weight, date, lbs, true);
+		req2.addEventListener("load", function(){
+			if (req2.status >= 200 && req.status < 400){
+				var res = req2.response;					
 				document.getElementById("response").textContent = res;
 			} else {
 				console.log("error");
 			}
 		});
-		req.send(null);
+		req2.send(null);
 		event.preventDefault();
 	});
 	event.preventDefault();
