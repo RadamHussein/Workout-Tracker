0a105e79e85035a4ad9a3308046411e3c1f52697
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 13:10:36 -0500

    added insert even to front end

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -10,17 +10,17 @@ function main(){
 	});
 	req.send(null);
 
-/*
+
 document.getElementById("add").addEventListener("click", function(event){
 		console.log("click is working");
 		var name = document.getElementById("name").value;
 		var req = new XMLHttpRequest();
-		req.open("GET", "http://52.33.123.66:3000/insert", name, false);
+		req.open("GET", "http://52.33.123.66:3000/insert", name, reps, weight, date, lbs, true);
 		req.addEventListener("load", function(){
 			var res = req.response;					
 			document.getElementById("response").textContent = res;
 		});
+		req.send(null);
 		event.preventDefault();
 	});
-*/
 };
