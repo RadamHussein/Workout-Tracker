b19efc7ef84fa6805a1033e8bf83d7e109aa185e
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 13:25:36 -0500

    trying req.withCredentials

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", main);
 
 function main(){
 	var req = new XMLHttpRequest();
+	req.withCredentials = true;
 	req.open("GET", "http://52.33.123.66:3000/", true);
 	req.addEventListener("load", function(){
 		var res = req.response;
