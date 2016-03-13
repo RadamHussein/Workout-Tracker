1dadae19f6305152a9003cabfd27726cc7e679b5
Adam Smith <stupidalphabet@gmail.com>
2016-03-13 13:41:54 -0500

    moved preventDefault

--- workoutFrontend.js
+++ workoutFrontend.js
@@ -16,16 +16,17 @@ document.getElementById("add").addEventListener("click", function(event){
 		console.log("click is working");
 		var name = document.getElementById("name").value;
 		var req = new XMLHttpRequest();
-		req.open("GET", "http://52.33.123.66:3000/insert" + name, reps, weight, date, lbs, true);
+		req.open("GET", "http://52.33.123.66:3000/insert", name, reps, weight, date, lbs, true);
 		req.addEventListener("load", function(){
 			if (req.status >= 200 && req.status < 400){
 				var res = req.response;					
 				document.getElementById("response").textContent = res;
 			} else {
-				console.log("401 error");
+				console.log("error");
 			}
 		});
 		req.send(null);
 		event.preventDefault();
 	});
+	event.preventDefault();
 };
