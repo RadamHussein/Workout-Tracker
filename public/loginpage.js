dd681e32646a68f1fea42e74f481eeb4f13c6034
Adam Smith <stupidalphabet@gmail.com>
2016-05-27 14:02:52 -0500

    checking front end response variable

--- public/loginpage.js
+++ public/loginpage.js
@@ -16,7 +16,7 @@ function bindButtons(){
 			if (logInRequest.status >= 200 && logInRequest.status < 400){
 				var response = logInRequest.responseText;
 				console.log(response);
-				console.log(response.results);	
+				console.log(response[0]);
 				if(response.results == true){	
 					window.open("http://52.33.123.66:2000/workout.html");
 				}
