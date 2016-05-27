0e725fe65823e6eebb6e572a2d7b84b39c83723f
Adam Smith <stupidalphabet@gmail.com>
2016-05-27 13:49:50 -0500

    change in logic for front end

--- public/loginpage.js
+++ public/loginpage.js
@@ -11,7 +11,7 @@ function bindButtons(){
 		var logInRequest = new XMLHttpRequest();
 		logInRequest.open("POST", "http://52.33.123.66:2000/logIn", true);
 		logInRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		logInRequest.addEventListener("load", function(event){
+		var openNewPage = logInRequest.addEventListener("load", function(event){
 			var goodResponse = true;
 			if (logInRequest.status >= 200 && logInRequest.status < 400){
 				var response = logInRequest.responseText;
@@ -20,10 +20,11 @@ function bindButtons(){
 				console.log("error");
 				goodResponse = false;
 			}
+			return goodResponse;
 		});
 		logInRequest.send(params);
 		event.preventDefault();
-		if(goodResponse == true){
+		if(openNewPage == true){
 			window.open("http://52.33.123.66:2000/workout.html");
 		}
 	});
