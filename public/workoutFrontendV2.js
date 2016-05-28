d5c61ec6635422f61766e7856a0d80936ba60dc7
Adam Smith <stupidalphabet@gmail.com>
2016-05-28 13:19:39 -0500

    basic front end testing

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -1,8 +1,64 @@
+document.addEventListener("DOMContentLoaded", main);
 
-function handleUserInfo(id, fname, lname){
-	//document.getElementById('user-name').textContent = fname + lname;
-	console.log(fname);
-	console.log(lname);
+function main(){
+	displayUsersTable();
+	//handleInsert();
 };
+
+//takes a single cell of table data and adds it to the current row
+function addTableRowToDOM(newRow, singleTableItem){
+	var cellData = document.createElement("td");
+	newRow.appendChild(cellData);
+	cellData.textContent = singleTableItem;
+};
+
+//add workouts to the table
+function convertWorkoutToTableRow(singleObjectRow){
+
+	//get cell data from each element in the object and add to DOM
+	var newRow = document.createElement("tr");
+	document.getElementById("tableBody").appendChild(newRow);
+	
+	//cut the time off the end of the date
+	//var formattedDate = singleObjectRow.date.slice(0, 10);
+
+	//send to function to add entry to table
+	addTableRowToDOM(newRow, singleObjectRow.fname);
+	addTableRowToDOM(newRow, singleObjectRow.lname);
+	addTableRowToDOM(newRow, singleObjectRow.usrename);
+	addTableRowToDOM(newRow, singleObjectRow.password);
+};
+
+//iterates through data returned form mysql and send individual objects to be added to table.
+function convertUsersToTable(objList){
+	for (var i=0; i<objList.results.length; i++){
+		convertUsersToTableRow(objList.results[i])
+	}
+};
+
+//gets users data from database 
+function getUsers(Users){
+	//calls get and returns a list of objects
+	var request = new XMLHttpRequest();
+	request.open("GET", "http://52.33.123.66:2000/getUsers", true);
+	request.withCredentials = true;
+	request.addEventListener("load", function(){
+		var response = request.responseText;
+		var objectListFromDatabase = JSON.parse(response); //change it to an object
+		console.log(typeof(objectListFromDatabase));
+		//Users(objectListFromDatabase); // Call the callback provided by the caller
+	});
+	request.send(null);
+};
+
+//gets called first. Calls getWorkouts and calls converWorkoutsToTable with results;
+function displayUsersTable(){
+	//Actually insert all of these construct elements into the DOM
+	console.log("Displaying users...");
+	getUsers(function Users(objectListFromDatabase){
+		//convertUsersToTable(objectListFromDatabase);
+	});
+};
+
 
 
