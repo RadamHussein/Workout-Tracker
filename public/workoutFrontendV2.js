59dc8b4886f2ea2524f87b5cff7af9b799e065aa
Adam Smith <stupidalphabet@gmail.com>
2016-05-28 12:12:59 -0500

    troubleshooting

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -1,6 +1,6 @@
 
 function handleUserInfo(id, fname, lname){
-	document.getElementById('user-name').textContent = fname + lname;
+	//document.getElementById('user-name').textContent = fname + lname;
 	console.log(fname);
 	console.log(lname);
 };
