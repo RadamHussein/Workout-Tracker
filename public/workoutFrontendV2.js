6af28a479875dc43b60bd3cb12933e450694eb38
Adam Smith <stupidalphabet@gmail.com>
2016-06-01 19:55:41 -0500

    fixed bug causing erroro message when requesting sets for an exercise when no sets exist

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -329,7 +329,7 @@ function getExerciseSets(){
 			resetTable("sets_table_body");
 
 			//if only build the table if there are sets for that exercise
-			if(newResponseObject.results[0] != null || newResponseObject.results[0] != undefined){
+			if(newResponseObject.results[0].weight != null){
 				convertSetsToTable(newResponseObject);
 			}
 
