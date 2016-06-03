c35bc301dd8ef579cfdeb2292f892c3cd78909a1
Adam Smith <stupidalphabet@gmail.com>
2016-06-03 15:42:36 -0500

    fixed typo causeing crash

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -157,7 +157,7 @@ function convertUsersToTableRow(singleObjectRow){
 		}
 		*/
 		//newRow.style.backgroundColor = "#FFFF8D";
-		newRow.setAttribute("style", "background-color: #FFFF8D;"):
+		newRow.setAttribute("style", "background-color: #FFFF8D;");
 		currentUser_Id = singleObjectRow.id;
 		currentWorkout_Id = null;
 		currentExercise_Id = null;
