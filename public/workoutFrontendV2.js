f73ab74801114a55c84792e7a7d5efc9ea278b9e
Adam Smith <stupidalphabet@gmail.com>
2016-06-03 15:16:49 -0500

    removing attribute trouble

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -157,7 +157,7 @@ function convertUsersToTableRow(singleObjectRow){
 		*/
 		//newRow.style.backgroundColor = "#FFFF8D";
 		var selectedRow = document.getElementsByClassName("clickable-row");
-		selectedRow.removeAttribute("clickable-row");
+		selectedRow.removeAttribute("class");
 		newRow.setAttribute("class", "clickable-row");
 		currentUser_Id = singleObjectRow.id;
 		currentWorkout_Id = null;
