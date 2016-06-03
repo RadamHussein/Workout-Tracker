db6ea9b430ab895da3f8f3b31c7be4f132d9677b
Adam Smith <stupidalphabet@gmail.com>
2016-06-03 15:41:47 -0500

    troublehsooting

--- public/workoutFrontendV2.js
+++ public/workoutFrontendV2.js
@@ -150,10 +150,12 @@ function convertUsersToTableRow(singleObjectRow){
 	newRow.addEventListener("click", function(event){
 		console.log("table row clicked");
 		
+		/*
 		var x = document.getElementsByTagName("TR");
 		for (var i = 0; i<x.length; i++){
 			x[i].style.backgroundColor = "white";
 		}
+		*/
 		//newRow.style.backgroundColor = "#FFFF8D";
 		newRow.setAttribute("style", "background-color: #FFFF8D;"):
 		currentUser_Id = singleObjectRow.id;
